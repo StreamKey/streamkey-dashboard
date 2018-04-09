e41ce1e6d98852dcebfd25aefbcc98d7d194dd97
Guy Brukhis <guy.br7@gmail.com>
2018-04-10 00:35:17 +0300

    wip add ssp one video

--- src/workers/scrapeDaily/GenerateAxiosCookies.js
+++ src/workers/scrapeDaily/GenerateAxiosCookies.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import axiosCookieJarSupport from 'axios-cookiejar-support'
 import tough from 'tough-cookie'
 
-export default () => {
+export default ({ returnJar }) => {
   const instance = axios.create({
     headers: {
       'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36'
@@ -12,5 +12,11 @@ export default () => {
   const cookieJar = new tough.CookieJar()
   instance.defaults.jar = cookieJar
   instance.defaults.withCredentials = true
+  if (returnJar === true) {
+    return {
+      axios: instance,
+      jar: cookieJar
+    }
+  }
   return instance
 }

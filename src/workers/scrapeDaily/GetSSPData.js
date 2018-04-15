56f7e7a194b5bfa0807fe98c62dab9ce6f7fbaba
Guy Brukhis <guy.br7@gmail.com>
2018-04-15 22:47:03 +0300

    add ssp aerserv

--- src/workers/scrapeDaily/GetSSPData.js
+++ src/workers/scrapeDaily/GetSSPData.js
@@ -1,11 +1,13 @@
 import Telaria from './SSP/Telaria'
 import Freewheel from './SSP/Freewheel'
 import Beachfront from './SSP/Beachfront'
+import Aerserv from './SSP/Aerserv'
 
 export default async dateTs => {
   const telariaData = await Telaria.getData(dateTs)
   const freewheelData = await Freewheel.getData(dateTs)
   const beachfrontData = await Beachfront.getData(dateTs)
+  const aerservData = await Aerserv.getData(dateTs)
   return [{
     key: 'telaria',
     data: telariaData
@@ -15,5 +17,8 @@ export default async dateTs => {
   }, {
     key: 'beachfront',
     data: beachfrontData
+  }, {
+    key: 'aerserv',
+    data: aerservData
   }]
 }
---
+++ src/workers/scrapeDaily/SSP/Aerserv.js
@@ -0,0 +1,58 @@
+import moment from 'moment'
+import csv from 'csvtojson'
+
+import GenerateAxiosCookies from '../GenerateAxiosCookies'
+
+const axios = GenerateAxiosCookies()
+
+const credentials = {
+  hashKey: process.env.RAZZLE_CREDENTIALS_AERSERV_KEY,
+  username: process.env.RAZZLE_CREDENTIALS_AERSERV_USERNAME
+}
+
+axios.defaults.baseURL = 'https://platform.aerserv.com/site_reports/export'
+
+const getResults = dateTs => {
+  return new Promise(async (resolve, reject) => {
+    const date = moment.utc(dateTs, 'X').format('MM/DD/YYYY')
+    const form = {
+      report_from_date: date,
+      report_to_date: date,
+      hash: credentials.hashKey,
+      username: credentials.username,
+      report_type: 'canned_placement',
+      no_export_headers: 0
+    }
+    try {
+      const res = await axios.get('/', { params: form })
+      const csvStr = res.data
+      csv()
+        .fromString(csvStr)
+        .on('end_parsed', jsonArr => {
+          resolve(jsonArr)
+        })
+    } catch (e) {
+      console.error(e)
+      reject(new Error('Aerserv report failed', e))
+    }
+  })
+}
+
+const normalize = results => {
+  return results.map(r => {
+    return {
+      tag: r['Placement'],
+      opp: Number(r['Requests']),
+      imp: Number(r['Impressions']),
+      rev: Number(r['Estimated Earnings'].replace(/^\$/, '')),
+      cpm: Number(r['eCPM'].replace(/^\$/, ''))
+    }
+  })
+}
+
+export default {
+  getData: async dateTs => {
+    const results = await getResults(dateTs)
+    return normalize(results)
+  }
+}

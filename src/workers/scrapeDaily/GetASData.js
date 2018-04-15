290547aa422d4744781b0e873c38d39fc4dbd7c1
Guy Brukhis <guy.br7@gmail.com>
2018-04-16 00:36:59 +0300

    add aniview

--- src/workers/scrapeDaily/GetASData.js
+++ src/workers/scrapeDaily/GetASData.js
@@ -1,14 +1,19 @@
 import StreamRail from './AS/StreamRail'
 import Lkqd from './AS/Lkqd'
+import Aniview from './AS/Aniview'
 
 export default async dateTs => {
   const streamRailData = await StreamRail.getData(dateTs)
   const lkqdData = await Lkqd.getData(dateTs)
+  const aniviewData = await Aniview.getData(dateTs)
   return [{
     key: 'streamRail',
     data: streamRailData
   }, {
     key: 'lkqd',
     data: lkqdData
+  }, {
+    key: 'aniview',
+    data: aniviewData
   }]
 }

28150421e8217aa32e5e665a407ea89be689b3a3
Guy Brukhis <guy.br7@gmail.com>
2018-04-17 01:00:04 +0300

    add as springserve

--- src/workers/scrapeDaily/AS/SpringServe.js
+++ src/workers/scrapeDaily/AS/SpringServe.js
@@ -1,1 +1,68 @@
-// TODO in a later stage
+import moment from 'moment'
+
+import GenerateAxiosCookies from '../GenerateAxiosCookies'
+
+const axios = GenerateAxiosCookies()
+
+const credentials = {
+  username: process.env.RAZZLE_CREDENTIALS_SPRINGSERVE_USERNAME,
+  password: process.env.RAZZLE_CREDENTIALS_SPRINGSERVE_PASSWORD
+}
+
+axios.defaults.baseURL = 'https://video.springserve.com'
+
+const login = async () => {
+  const form = {
+    email: credentials.username,
+    password: credentials.password
+  }
+  try {
+    const res = await axios.post('/api/v0/auth', form)
+    axios.defaults.headers.common['Authorization'] = res.data.token
+  } catch (e) {
+    console.error(e)
+    throw new Error('SpringServe login failed', e)
+  }
+}
+
+const getResults = async dateTs => {
+  const date = moment.utc(dateTs, 'X').format('YYYY-MM-DD')
+  const form = {
+    date_range: 'Last 7 Days',
+    start_date: date,
+    end_date: date,
+    interval: 'day',
+    timezone: 'UTC',
+    dimensions: [
+      // 'supply_tag_id',
+      'demand_tag_id'
+    ]
+  }
+  try {
+    const res = await axios.post('/api/v0/report', form)
+    return res.data
+  } catch (e) {
+    console.error(e)
+    throw new Error('SpringServe report failed', e)
+  }
+}
+
+const normalize = results => {
+  return results.map(r => {
+    return {
+      tag: r.demand_tag_name,
+      opp: r.opportunities,
+      imp: r.impressions,
+      rev: r.revenue,
+      cpm: r.cpm
+    }
+  })
+}
+
+export default {
+  getData: async dateTs => {
+    await login()
+    const results = await getResults(dateTs)
+    return normalize(results)
+  }
+}
--- src/workers/scrapeDaily/GetASData.js
+++ src/workers/scrapeDaily/GetASData.js
@@ -1,11 +1,13 @@
 import StreamRail from './AS/StreamRail'
 import Lkqd from './AS/Lkqd'
 import Aniview from './AS/Aniview'
+import SpringServe from './AS/SpringServe'
 
 export default async dateTs => {
   const streamRailData = await StreamRail.getData(dateTs)
   const lkqdData = await Lkqd.getData(dateTs)
   const aniviewData = await Aniview.getData(dateTs)
+  const springserveData = await SpringServe.getData(dateTs)
   return [{
     key: 'streamRail',
     data: streamRailData
@@ -15,5 +17,8 @@ export default async dateTs => {
   }, {
     key: 'aniview',
     data: aniviewData
+  }, {
+    key: 'springserve',
+    data: springserveData
   }]
 }

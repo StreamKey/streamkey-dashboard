5823724c856da34233df9a7862369787d493438d
Guy Brukhis <guy.br7@gmail.com>
2018-05-07 23:36:21 +0300

    log list style

--- src/pages/Logs.js
+++ src/pages/Logs.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import { withStyles } from 'material-ui/styles'
 import moment from 'moment'
-import { omit } from 'lodash'
+import { omit, orderBy, take } from 'lodash'
+
+import Button from 'material-ui/Button'
+import IconButton from 'material-ui/IconButton'
+import List, {
+  ListItem,
+  ListItemText,
+  ListItemIcon
+} from 'material-ui/List'
+import FileSvg from 'mdi-svg/svg/file-outline.svg'
 
 import LogViewer from '../components/Log/LogViewer'
+import MdIcon from '../components/MdIcon'
 import API from '../components/API'
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
@@ -25,11 +35,17 @@ const styles = theme => {
       paddingTop: theme.spacing.quad,
       paddingBottom: theme.spacing.double
     },
-    link: {
-      cursor: 'pointer',
-      '&:hover': {
-        textDecoration: 'underline'
-      }
+    logItem: {
+      padding: 0
+    },
+    logButton: {
+      height: theme.spacing.quad,
+      width: theme.spacing.quad,
+      marginRight: 0
+    },
+    logButtonIcon: {
+      height: theme.spacing.double,
+      width: theme.spacing.double
     }
   }
 }
@@ -39,8 +55,10 @@ class Logs extends React.Component {
     super(props)
     this.state = {
       logsList: [],
-      currentLog: []
+      currentLog: [],
+      showAll: false
     }
+    this.logRef = null
   }
 
   componentDidMount () {
@@ -51,7 +69,7 @@ class Logs extends React.Component {
     const response = await API.get('/logs')
     this.setState({
       ...this.state,
-      logsList: response.data.logs
+      logsList: orderBy(response.data.logs, [], ['desc'])
     })
   }
 
@@ -73,35 +91,61 @@ class Logs extends React.Component {
       this.setState({
         ...this.state,
         currentLog
-      })
+      }, this.scrollShowLog)
     } catch (e) {
       console.error('Invalid log format')
       console.error(e)
     }
   }
 
+  toggleShowAll = () => {
+    this.setState({
+      ...this.state,
+      showAll: !this.state.showAll
+    })
+  }
+
+  setLogRef = element => {
+    this.logRef = element
+  }
+
+  scrollShowLog = () => {
+    if (this.logRef) {
+      this.logRef.scrollIntoView()
+    }
+  }
+
   render () {
     const { classes } = this.props
-    const { currentLog } = this.state
+    const { currentLog, logsList, showAll } = this.state
     return (
       <div className={classes.root}>
         <NavBar />
         <div className={classes.container}>
           <h3 className={classes.title}>Logs</h3>
-          <ul>
+          <List dense>
             {
-              this.state.logsList.map(l => (
-                <li key={l}>
-                  <a
-                    className={classes.link}
-                    onClick={this.getLogData(l)}>{l}</a>
-                </li>
+              (showAll ? logsList : take(logsList, 10)).map(l => (
+                <ListItem key={l} className={classes.logItem}>
+                  <ListItemIcon>
+                    <IconButton
+                      onClick={this.getLogData(l)}
+                      className={classes.logButton}
+                    >
+                      <MdIcon svg={FileSvg} className={classes.logButtonIcon} />
+                    </IconButton>
+                  </ListItemIcon>
+                  <ListItemText primary={l} />
+                </ListItem>
               ))
             }
-          </ul>
-          {
-            currentLog.length > 0 && <LogViewer rows={currentLog} />
-          }
+          </List>
+          <Button size='small' onClick={this.toggleShowAll}>
+            { showAll ? 'Show less' : `+ ${logsList.length - 10}` }
+          </Button>
+          <div ref={this.setLogRef}>
+            <LogViewer rows={currentLog} />
+          </div>
         </div>
         <Footer />
       </div>

761fcc84be0731f5d268382c83016eb87525a2eb
Guy Brukhis <guy.br7@gmail.com>
2018-05-08 23:01:20 +0300

    upload ftp file to legacy server

--- src/api/controllers/LegacyFtp/runLkqdTremorDuplicate.js
+++ src/api/controllers/LegacyFtp/runLkqdTremorDuplicate.js
@@ -1,3 +1,44 @@
-export default async () => {
-  return false
+import fs from 'fs'
+import path from 'path'
+import ssh2 from 'ssh2'
+
+import { promisify } from 'util'
+const SshClient = ssh2.Client
+
+const {
+  RAZZLE_LEGACY_USER,
+  RAZZLE_LEGACY_PASSWORD,
+  RAZZLE_LEGACY_HOST,
+  RAZZLE_LEGACY_SSH_KEY
+} = process.env
+
+const LOCAL_FILE_PATH = path.join(__dirname, 'run.txt')
+const REMOTE_FULL_FILE_PATH = `/home/${RAZZLE_LEGACY_USER}/dev/tremor/run.txt`
+
+export default () => {
+  return new Promise((resolve, reject) => {
+    const conn = new SshClient()
+    conn.on('ready', () => {
+      conn.sftp(async (err, sftp) => {
+        if (err) {
+          reject(err)
+        }
+        const fastPut = promisify(sftp.fastPut.bind(sftp))
+        try {
+          await fastPut(LOCAL_FILE_PATH, REMOTE_FULL_FILE_PATH)
+          conn.end()
+          resolve(true)
+        } catch (e) {
+          conn.end()
+          reject(e)
+        }
+      })
+    }).connect({
+      host: RAZZLE_LEGACY_HOST,
+      port: 22,
+      username: RAZZLE_LEGACY_USER,
+      password: RAZZLE_LEGACY_PASSWORD,
+      privateKey: fs.readFileSync(RAZZLE_LEGACY_SSH_KEY)
+    })
+  })
 }

135ee15f114664b14f3bf31ffd614c8298aed72e
Guy Brukhis <guy.br7@gmail.com>
2018-11-04 10:42:16 +0200

    tappx fetch apps list

--- src/workers/scrapeDaily/SSP/Tappx.js
+++ src/workers/scrapeDaily/SSP/Tappx.js
@@ -1,12 +1,50 @@
 import moment from 'moment'
 import axios from 'axios'
+import qs from 'querystring'
+
+import GenerateAxiosCookies from '../GenerateAxiosCookies'
+
+const axiosInstance = GenerateAxiosCookies()
 
 const credentials = {
   userId: process.env.RAZZLE_CREDENTIALS_TAPPX_USERID,
-  apiKey: process.env.RAZZLE_CREDENTIALS_TAPPX_APIKEY
+  apiKey: process.env.RAZZLE_CREDENTIALS_TAPPX_APIKEY,
+  username: process.env.RAZZLE_CREDENTIALS_TAPPX_USERNAME,
+  password: process.env.RAZZLE_CREDENTIALS_TAPPX_PASSWORD
 }
 
-const baseURL = 'http://reporting.api.tappx.com/ssp/v3'
+const apiBaseURL = 'http://reporting.api.tappx.com/ssp/v3'
+const webBaseURL = 'https://www.tappx.com/en/admin'
+
+const login = async () => {
+  const form = {
+    username: credentials.username,
+    password: credentials.password
+  }
+  try {
+    await axiosInstance.post(webBaseURL + '/login/', qs.stringify(form))
+  } catch (e) {
+    e.prevError = e.message
+    e.message = 'Tappx login failed'
+    throw e
+  }
+}
+
+const getApps = async () => {
+  try {
+    const res = await axiosInstance.get(webBaseURL + '/api/reports/userdata/?type=monetize')
+    const apps = res.data['filter'].filter(i => i.id === 'app')[0].childs
+    const appDict = {}
+    for (let a of apps) {
+      appDict[a.id] = a.name.replace(/^\d+\s*-\s*/, '')
+    }
+    return appDict
+  } catch (e) {
+    e.prevError = e.message
+    e.message = 'Tappx get apps failed'
+    throw e
+  }
+}
 
 const getResults = async dateTs => {
   try {
@@ -18,8 +56,8 @@ const getResults = async dateTs => {
       date_end: date,
       currency: 'USD'
     }
-    const res = await axios.get(baseURL, { params })
-    const data = res.data.split('\n').map(line => line.split(';'))
+    const res = await axios.get(apiBaseURL, { params })
+    const data = res.data.replace(/[\n\r\s]*$/, '').split('\n').map(line => line.split(';'))
     return data
   } catch (e) {
     e.prevError = e.message
@@ -28,10 +66,11 @@ const getResults = async dateTs => {
   }
 }
 
-const normalize = results => {
+const normalize = (results, apps) => {
   return results.map(r => {
+    const appName = apps[r[1]] || 'Unknown Tappx app: ' + r[1]
     return {
-      tag: r[1], // TappxID. Should be app name
+      tag: appName,
       opp: r[6],
       imp: r[8],
       rev: r[10],
@@ -42,7 +81,9 @@ const normalize = results => {
 
 export default {
   getData: async dateTs => {
+    await login()
+    const apps = await getApps()
     const results = await getResults(dateTs)
-    return normalize(results)
+    return normalize(results, apps)
   }
 }

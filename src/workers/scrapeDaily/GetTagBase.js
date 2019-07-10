122c4fe920629de3765a26c8b5c8687d86631d7b
Guy Brukhis <guy.br7@gmail.com>
2019-07-10 20:19:42 +0300

    convert tag base to upper case to make tag comparison case insensitive

--- src/workers/scrapeDaily/GetTagBase.js
+++ src/workers/scrapeDaily/GetTagBase.js
@@ -1,6 +1,6 @@
 export default tag => {
   const re = /(?:MNL_|AUTON_|)(.*?\d+\.\d+)/ig
-  const matches = re.exec(tag)
+  const matches = re.exec(tag.toUpperCase())
   if (matches) {
     return matches[1]
   } else {
